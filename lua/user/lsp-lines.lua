88c5e6ce5aa7ab77b0b22ab134e413cf47c63685
alexi <30680965+alexisharara@users.noreply.github.com>
2022-12-15 00:03:09 -0500

    fix lsp-lines

--- lua/user/lsp-lines.lua
+++ lua/user/lsp-lines.lua
@@ -1,3 +1,10 @@
+local status_ok, lsp_lines = pcall(require, "lsp_lines")
+if not status_ok then
+  return
+end
+
+lsp_lines.setup()
+
 vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
 vim.api.nvim_create_autocmd('InsertEnter', {
     callback = function()
